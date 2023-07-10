d3f628f5ab6a14eeb20bbb683371f7e1ac871028
Gaelen Carroll <gaelencarroll02@gmail.com>
2023-07-10 15:48:52 -0400

    updated with part three

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag
 
 
 app = Flask(__name__)
@@ -93,7 +93,7 @@ def users_update(user_id):
 
 
 @app.route('/users/<int:user_id>/delete', methods=["POST"])
-def users_destroy(user_id):
+def users_delete(user_id):
     """Handle form submission for deleting an existing user"""
 
     user = User.query.get_or_404(user_id)
@@ -107,14 +107,17 @@ def users_destroy(user_id):
 @app.route('/users/<int:user_id>/posts/new')
 def posts_new_form(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template('posts/new.html', user=user)
+    tags = Tag.query.all()
+    return render_template('posts/new.html', user=user, tags=tags)
 
 
 @app.route('/users/<int:user_id>/posts/new', methods=['POST'])
 def posts_new(user_id):
     user = User.query.get_or_404(user_id)
+    tag_ids = [int(num) for num in request.form.getlist('tags')]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
     post_new = Post(
-        title=request.form['title'], post_content=request.form['post_content'], user=user)
+        title=request.form['title'], post_content=request.form['post_content'], user=user, tags=tags)
     db.session.add(post_new)
     db.session.commit()
     flash(f"Post '{post_new.title}' added")
@@ -130,7 +133,8 @@ def posts_page(post_id):
 @app.route('/posts/<int:post_id>/edit')
 def posts_edit_form(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template('posts/edit.html', post=post)
+    tags = Tag.query.all()
+    return render_template('posts/edit.html', post=post, tags=tags)
 
 
 @app.route('/posts/<int:post_id>/edit', methods=['POST'])
@@ -138,6 +142,8 @@ def posts_update(post_id):
     post = Post.query.get_or_404(post_id)
     post.title = request.form['title']
     post.post_content = request.form['post_content']
+    tag_id = [int(num) for num in request.form.getlist('tags')]
+    post.tags = Tag.query.filter(Tag.id.in_(tag_id)).all()
     db.session.add(post)
     db.session.commit()
     return redirect(f"/users/{post.user_id}")
@@ -150,6 +156,60 @@ def posts_delete(post_id):
     db.session.commit()
     flash(f"Post '{post.title}' deleted")
     return redirect(f'/users/{post.user_id}')
+
+
+@app.route('/tags')
+def tags_page():
+    tags = Tag.query.all()
+    return render_template('tags/index.html', tags=tags)
+
+
+@app.route('/tags/new')
+def tags_new():
+    posts = Post.query.all()
+    return render_template('tags/new.html', posts=posts)
+
+
+@app.route('/tags/new', methods=['POST'])
+def tags_new_submit():
+    post_ids = [int(num) for num in request.form.getlist('posts')]
+    posts = Post.query.filter(Post.id.in_(post_ids)).all()
+    tag_new = Tag(name=request.form['name'], posts=posts)
+    db.session.add(tag_new)
+    db.session.commit()
+    return redirect('/tags')
+
+
+@app.route('/tags/<int:tag_id>')
+def tags_show(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('tags/show.html', tag=tag)
+
+
+@app.route('/tags/<int:tag_id>/edit')
+def tags_edit(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    posts = Post.query.all()
+    return render_template('tags/edit.html', tag=tag, posts=posts)
+
+
+@app.route('/tags/<int:tag_id>/edit', methods=['POST'])
+def tags_edit_submit(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    post_ids = [int(num) for num in request.form.getlist('posts')]
+    tag.name = request.form['name']
+    tag.posts = Post.query.filter(Post.id.in_(post_ids)).all()
+    db.session.add(tag)
+    db.session.commit()
+    return redirect('/tags')
+
+
+@app.route('/tags/<int:tag_id>/delete', methods=['POST'])
+def tags_delete(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+    return redirect('/tags')
 
 
 @app.errorhandler(404)
